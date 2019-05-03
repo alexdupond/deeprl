f4b1d722e1cb0155dab4596cd5e899a88e198128
Rasmus Haugaard <rasmus.l.haugaard@gmail.com>
2019-05-03 15:47:14 +0200

    same max_episode_steps sim real

--- src/gym_ros_reacher/gym_ros_reacher_env/__init__.py
+++ src/gym_ros_reacher/gym_ros_reacher_env/__init__.py
@@ -6,11 +6,11 @@ logger = logging.getLogger(__name__)
 register(
     id='ROSDynReacherVelChange-v0',
     entry_point='gym_ros_reacher_env.envs:ROSDynEnv',
-    max_episode_steps=75,
+    max_episode_steps=200,
 )
 
 register(
     id='ROSForce-v0',
     entry_point='gym_ros_reacher_env.envs:ROSDynForceEnv',
-    max_episode_steps=75,
+    max_episode_steps=200,
 )
